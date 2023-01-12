3742d5c43f10d31a2dea12f01765b9a324d0ece4
qtsu <118324529+qtsu@users.noreply.github.com>
2023-01-12 10:46:56 +0900

    20230112

--- project001/js/js.js
+++ project001/js/js.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // nav를 클릭했을 때 submenu가 보임
   $('nav li').mouseenter(function(){
     let i = $(this).index()
     console.log(i)
@@ -8,4 +9,35 @@ $(document).ready(function(){
   $('header').mouseleave(function(){
     $('.submenu>ul').slideUp()
   });
+
+  // submenu를 클릭했을 때 글씨 색상 변경
+  $('submenu a').click(function(){
+    let txt= $(this).text()
+    console.log(txt)
+    $('.wrap').removeClass()
+    $('.wrap').addClass(txt)
+})
+
+  // btn을 클릭했을 때 slide img가 이동
+  $('.btn li').click(function(){
+    let i = $(this).index();
+    $('.slide .img').eq(i-1).css({'left':'0'}).animate({'left':'-100%'});
+    $('.slide .img').eq(i).css({'left':'100%'}).animate({'left':'0'});
+    $('.btn li').removeClass('on');
+    $(this).addClass('on');
+  });
+  // 일정시간 간격으로 btn을 순차적으로 자동 클릭
+  let btn = 0
+  let slide = setInterval(timer,2000);
+  $('.wrap').mouseenter(function(){
+    clearInterval(slide)
+  });
+  $('.wrap').mouseleave(function(){
+    slide = setInterval(timer,2000);
+  });
+  function timer(){
+    a++;
+    if(btn==3)a=0;
+    $('.btn li').eq(a).trigger('click')
+  }
 });
