e51b0c6ce223c4fe5b7cc455476eeb4b0ad1ba77
qtsu <118324529+qtsu@users.noreply.github.com>
2023-01-12 10:08:00 +0900

    20230112

--- example/photo/js/js.js
+++ example/photo/js/js.js
@@ -4,36 +4,47 @@ $(document).ready(function(){
   let a = $('article').size();
   let awd = $('article').width();
   $('section').width((a*(awd+20))+600);
-  $('body').height((a*(awd+20))+600);
+  $('body').height(a*(awd+20));
+  $('html,body').scrollTop(a*(awd+20));
   $(window).resize(function(){
   // 화면이 resize 될 때마다, body 높이값과 section의 가로값 통일시키기
   // article개수를 구하고 article의 가로값을 구해서 두 개를 곱한 값을 section의 가로값으로 변환
   let a = $('article').size();
   let aWd= $('article').width()
   $('section').width((a*(awd+20))+600);
-  $('body').height((a*(awd+20))+600);
+  $('body').height((a*(awd+20)));
   });
 
   // 화면에서 스크롤바가 움직일 때 상단의 위치값을 찾아라.
   $(window).scroll(function(){
     let sc = $(window).scrollTop();
-    // $('h1').text(sc);
+    $('h1').text(sc);
     $('section').stop().animate({'left':-sc},600);
   });
   $('.gnb li').click(function(){
     let i = $(this).index();
-    $('html,body').scrollTop(1000*i)
+    $('html,body').scrollTop(1000*i);
   });
 
   // article을 클릭했을 때 내가 클릭한 그 아이에게 addClass를 해라.
   // 모든 article에겐 removeClass를 먼저해라.
   $('article h2').click(function(e){
     e.preventDefault(); //기존에 있었던 a의 이벤트값을 없애라.
-    $('article').removeClass('on')
-    $(this).parent().addClass('on')
+
+    // 클릭한 나의 부모자의 순번을 찾아라.
+    let id = $(this).parent().index();
+    // 클릭한 나의 자손인 'a'의 속성값
+    let src = $(this).children('a').attr('href');
+    $('article p img').attr({'src':''});
+    // 클릭한 나의 형제인 'p'의 자손인 'img' 안에 속성명 src 안에 대입해라.
+    $(this).siblings('p').children('img').attr({'src':src});
+
+    $('article').removeClass('on');
+    $(this).parent().addClass('on');
+    $('html,body').scrollTop(200*id);
   });
   // span을 클릭했을 때 article에 removeClass를 해라.
   $('article span').click(function(){
-    $(this).parent().removeClass('on')
+    $(this).parent().removeClass('on');
   });
 });
